969187aef4a966efa5616a120bfbd879482d45d8
butatensei <31736483+butatensei@users.noreply.github.com>
2018-03-18 16:50:26 +0100

    Update met goede csv files
    Python bestand staat er nog in plus de twee files die het gebruikt, mocht het nog van pas komen.

--- Fix this csv.py
+++ Fix this csv.py
@@ -1,42 +1,47 @@
-fileIn1 = 'fietsdiefstal-rotterdam-2011-2013.csv'
+fileIn1 = 'KNMI_20180316_hourly.csv'
 fileOut1 = 'FixedThatForYou.csv'
-column1 = 11
+column1 = 1
 column2 = 0
-KNMI = False
-
-weatherStations = 'Weerstations.csv'
-
+KNMI = True
 
 import csv
 
-
-if KNMI:
-    with open(weatherStations) as csvweatherStations:
+stationDict = {}
+typeDict = {}
+weatherStations = 'Weerstations.csv'
+columnTypes = 'Kolommen.csv'
+with open(weatherStations) as csvweatherStations:
+    with open(columnTypes) as csvcolumnTypes:
         weatherReader = csv.reader(csvweatherStations)
+        typeReader = csv.reader(csvcolumnTypes)
         for row in weatherReader:
-            if row[0] == row2[0]:
-                row[0] = row2[4]
+            stationDict[row[0]] = row[4]
+        for row in typeReader:
+            typeDict[row[0]] = row[1]
 
-with open(fileIn1) as csvfileIn1:
-    
-    csvfileIn1.seek(0)
-    reader = csv.reader(csvfileIn1)
+        with open(fileIn1) as csvfileIn1:
 
+            csvfileIn1.seek(0)
+            reader = csv.reader(csvfileIn1)
 
+            for row in reader:
 
-    for row in reader:
+                if row[column1][2] == "/":
+                    row[column1] = row[column1].replace("/", "")
+                    date = [row[column1][:2], row[column1][2:4], row[column1][4:]]
+                else:
+                    date = [row[column1][:4], row[column1][4:6], row[column1][6:]]
+                row = row[:column1] + date + row[(column1+1):]
+                for i in range(len(row)):
+                    row[i] = row[i].strip(" ")
+                for i in range(len(row)):
+                    if row[i] in typeDict:
+                        row[i] = typeDict[row[i]]
+                if row[0] in stationDict:
+                    row[0] = stationDict[row[0]]
 
-        if row[column1][2] == "/":
-            row[column1] = row[column1].replace("/", "")
-            date = [row[column1][:2], row[column1][2:4], row[column1][4:]]
-        else:
-            date = [row[column1][:4], row[column1][4:6], row[column1][6:]]
-        row = row[:(column1-1)] + date + row[(column1+1):]
+                print(row)
 
-
-
-        print(row)
-
-        with open(fileOut1, 'a',  newline='') as csvfileOut1:
-            writer = csv.writer(csvfileOut1)
-            writer.writerow(row)
+                with open(fileOut1, 'a',  newline='') as csvfileOut1:
+                    writer = csv.writer(csvfileOut1)
+                    writer.writerow(row)
