61a48fada913e1632efd69be1a931a471b02f4f3
SonixApache <soniixapache@hotmail.com>
2018-03-21 16:18:10 +0100

    preliminary mySQL stuff
    copied over an example from the net and commented out the rest of the code

--- P3G1DBP-SG1/Program.cs
+++ P3G1DBP-SG1/Program.cs
@@ -1,29 +1,87 @@
 ﻿using System;
 using MySql.Data.MySqlClient;
-using Npgsql;
 
 namespace Project3Groep1
 {
     class DBP
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Setting up connection...");
-            Console.WriteLine("Enter username...");
-            var myUserName = Console.ReadLine();
-            Console.WriteLine("Enter password...");
-            var myPassword = Console.ReadLine();
-            var myConnection = ConnectToDatabase(myUserName, myPassword);
-            Console.WriteLine("Connection set up.");
-            Console.WriteLine("Enter SQL statement...");
-            var myStatement = Console.ReadLine();
-            var myReturn = EnterQuery(myStatement, myConnection);
-            Console.WriteLine(myReturn);
-            Console.WriteLine("Program complete.");
-            Console.WriteLine("Press any key to shut down...");
-            Console.ReadLine();
-        }
 
+
+
+        class DBConnect
+        {
+            private MySqlConnection connection;
+            private string server;
+            private string database;
+            private string uid;
+            private string password;
+
+            //Constructor
+            public DBConnect()
+            {
+                Initialize();
+            }
+
+            //Initialize values
+            private void Initialize()
+            {
+                server = "localhost";
+                database = "connectcsharptomysql";
+                uid = "username";
+                password = "password";
+                string connectionString;
+                connectionString = "SERVER=" + server + ";" + "DATABASE=" +
+                database + ";" + "UID=" + uid + ";" + "PASSWORD=" + password + ";";
+
+                connection = new MySqlConnection(connectionString);
+            }
+
+            //open connection to database
+            private bool OpenConnection()
+            {
+            }
+
+            //Close connection
+            private bool CloseConnection()
+            {
+            }
+
+            //Insert statement
+            public void Insert()
+            {
+            }
+
+            //Update statement
+            public void Update()
+            {
+            }
+
+            //Delete statement
+            public void Delete()
+            {
+            }
+
+            //Select statement
+            public List<string>[] Select()
+            {
+            }
+
+            //Count statement
+            public int Count()
+            {
+            }
+
+            //Backup
+            public void Backup()
+            {
+            }
+
+            //Restore
+            public void Restore()
+            {
+            }
+        }
+        /*
         /// <summary>
         /// Sets up a connection to a database.
         /// </summary>
@@ -40,21 +98,21 @@ namespace Project3Groep1
             );
             return connection;
         }
-
-
+        
+        
         /// <summary>
         /// Executed a statement on the connected server
         /// </summary>
         /// <param name="myUser">The username for the database.</param>
         /// <param name="myPassword">The password for the database.</param>
         /// <returns>NpgsqlConnection</returns>
-        /*public string EnterQuery(string myStatement, NpgsqlConnection myConnection)
+        public string EnterQuery(string myStatement, NpgsqlConnection myConnection)
         {
             myConnection.Open();
             var myReturn = NpgsqlCommand(myStatement, myConnection);
             myConnection.Close();
             return myReturn;
-        }*/
+        }
 
         static bool EnterQuery(string myStatement, NpgsqlConnection myConnection)
         {
@@ -66,21 +124,23 @@ namespace Project3Groep1
             myConnection.Close();
             return true;
         }
-
-        //static NpgsqlConnection ConnectToDatabase(string myUser, string myPassword)
-        /*
-        // Open database connection.
-        static void openConnection()
-        {
-           connection.Open();
-        }
-
-        // Close database connection.
-        private void closeConnection()
-        {
-            connection.Close();
-        }
         */
-
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Setting up connection...");
+            Console.WriteLine("Enter username...");
+            //var myUserName = Console.ReadLine();
+            Console.WriteLine("Enter password...");
+            //var myPassword = Console.ReadLine();
+            //var myConnection = ConnectToDatabase(myUserName, myPassword);
+            Console.WriteLine("Connection set up.");
+            Console.WriteLine("Enter SQL statement...");
+            //var myStatement = Console.ReadLine();
+            //var myReturn = EnterQuery(myStatement, myConnection);
+            Console.WriteLine(myReturn);
+            Console.WriteLine("Program complete.");
+            Console.WriteLine("Press any key to shut down...");
+            Console.ReadLine();
+        }
     }
 }
