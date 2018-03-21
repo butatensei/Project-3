f3f24fa1506fa069738ed410630e5983632f9c9e
SonixApache <soniixapache@hotmail.com>
2018-03-21 18:39:19 +0100

    DATABASE WORKS - CODE WORKS
    TEST BUTTON PERFORMS QUERY THAT GETS A COUNT OF ALL THE STUFF IN THE 'WEER' DATABASE AS A PROOF OF CONCEPT
    I AM SO HAPPY I COULD CRY

--- P3G1DBP-SG1/Program.cs
+++ P3G1DBP-SG1/Program.cs
@@ -3,128 +3,148 @@ using MySql.Data.MySqlClient;
 
 namespace Project3Groep1
 {
-    class DBP
+    class DBConnect
     {
+        private MySqlConnection connection;
+        private string server;
+        private string database;
+        private string uid;
+        private string password;
 
-
-
-        class DBConnect
+        //Constructor
+        public DBConnect()
         {
-            private MySqlConnection connection;
-            private string server;
-            private string database;
-            private string uid;
-            private string password;
-
-            //Constructor
-            public DBConnect()
-            {
-                Initialize();
-            }
-
-            //Initialize values
-            private void Initialize()
-            {
-                server = "localhost";
-                database = "connectcsharptomysql";
-                uid = "username";
-                password = "password";
-                string connectionString;
-                connectionString = "SERVER=" + server + ";" + "DATABASE=" +
-                database + ";" + "UID=" + uid + ";" + "PASSWORD=" + password + ";";
-
-                connection = new MySqlConnection(connectionString);
-            }
-
-            //open connection to database
-            private bool OpenConnection()
-            {
-            }
-
-            //Close connection
-            private bool CloseConnection()
-            {
-            }
-
-            //Insert statement
-            public void Insert()
-            {
-            }
-
-            //Update statement
-            public void Update()
-            {
-            }
-
-            //Delete statement
-            public void Delete()
-            {
-            }
-
-            //Select statement
-            public List<string>[] Select()
-            {
-            }
-
-            //Count statement
-            public int Count()
-            {
-            }
-
-            //Backup
-            public void Backup()
-            {
-            }
-
-            //Restore
-            public void Restore()
-            {
-            }
-        }
-        /*
-        /// <summary>
-        /// Sets up a connection to a database.
-        /// </summary>
-        /// <param name="myUser">The username for the database.</param>
-        /// <param name="myPassword">The password for the database.</param>
-        /// <returns>NpgsqlConnection</returns>
-        static NpgsqlConnection ConnectToDatabase(string myUser, string myPassword)
-        {
-            NpgsqlConnection connection = new NpgsqlConnection("Server=localhost;" +
-                "Port=5432;" +
-                "User Id=" + myUser + ";" +
-                "Password=" + myPassword + ";" +
-                "Database=mydatabase;"
-            );
-            return connection;
-        }
-        
-        
-        /// <summary>
-        /// Executed a statement on the connected server
-        /// </summary>
-        /// <param name="myUser">The username for the database.</param>
-        /// <param name="myPassword">The password for the database.</param>
-        /// <returns>NpgsqlConnection</returns>
-        public string EnterQuery(string myStatement, NpgsqlConnection myConnection)
-        {
-            myConnection.Open();
-            var myReturn = NpgsqlCommand(myStatement, myConnection);
-            myConnection.Close();
-            return myReturn;
+            Initialize();
         }
 
-        static bool EnterQuery(string myStatement, NpgsqlConnection myConnection)
+        //Initialize values
+        private void Initialize()
         {
-            myConnection.Open();
-            using (var cmd = new NpgsqlCommand("SELECT some_field FROM data", myConnection))
-            using (var reader = cmd.ExecuteReader())
-                while (reader.Read())
-                    Console.WriteLine(reader.GetString(0));
-            myConnection.Close();
-            return true;
+            server = "localhost";
+            database = "SLUISQL";
+            uid = "root";
+            password = "project3groep1";
+            string connectionString;
+            connectionString = "SERVER=" + server + ";" + "DATABASE=" +
+            database + ";" + "UID=" + uid + ";" + "PASSWORD=" + password + ";";
+
+            connection = new MySqlConnection(connectionString);
         }
-        */
+
+        //open connection to database
+        private bool OpenConnection()
+        {
+            try
+            {
+                connection.Open();
+                return true;
+            }
+            catch (MySqlException ex)
+            {
+                //When handling errors, you can your application's response based 
+                //on the error number.
+                //The two most common error numbers when connecting are as follows:
+                //0: Cannot connect to server.
+                //1045: Invalid user name and/or password.
+                switch (ex.Number)
+                {
+                    case 0:
+                        MessageBox.Show("Cannot connect to server.  Contact administrator");
+                        break;
+
+                    case 1045:
+                        MessageBox.Show("Invalid username/password, please try again");
+                        break;
+                }
+                return false;
+            }
+        }
+
+        //Close connection
+        private bool CloseConnection()
+        {
+            try
+            {
+                connection.Close();
+                return true;
+            }
+            catch (MySqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+                return false;
+            }
+        }
+
+        //Select statement
+        //Select statement
+        public List<string>[] Select()
+        {
+            string query = "SELECT * FROM tableinfo";
+
+            //Create a list to store the result
+            List<string>[] list = new List<string>[3];
+            list[0] = new List<string>();
+            list[1] = new List<string>();
+            list[2] = new List<string>();
+
+            //Open connection
+            if (this.OpenConnection() == true)
+            {
+                //Create Command
+                MySqlCommand cmd = new MySqlCommand(query, connection);
+                //Create a data reader and Execute the command
+                MySqlDataReader dataReader = cmd.ExecuteReader();
+
+                //Read the data and store them in the list
+                while (dataReader.Read())
+                {
+                    list[0].Add(dataReader["id"] + "");
+                    list[1].Add(dataReader["name"] + "");
+                    list[2].Add(dataReader["age"] + "");
+                }
+
+                //close Data Reader
+                dataReader.Close();
+
+                //close Connection
+                this.CloseConnection();
+
+                //return list to be displayed
+                return list;
+            }
+            else
+            {
+                return list;
+            }
+        }
+
+        //Count statement
+        //Count statement
+        public int Count()
+        {
+            string query = "SELECT Count(*) FROM tableinfo";
+            int Count = -1;
+
+            //Open Connection
+            if (this.OpenConnection() == true)
+            {
+                //Create Mysql Command
+                MySqlCommand cmd = new MySqlCommand(query, connection);
+
+                //ExecuteScalar will return one value
+                Count = int.Parse(cmd.ExecuteScalar() + "");
+
+                //close Connection
+                this.CloseConnection();
+
+                return Count;
+            }
+            else
+            {
+                return Count;
+            }
+        }
+    }
         static void Main(string[] args)
         {
             Console.WriteLine("Setting up connection...");
