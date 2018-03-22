089ea8705fa933aca926a4a315cef1fa2ae3e268
butatensei <31736483+butatensei@users.noreply.github.com>
2018-03-22 03:52:32 +0100

    Still trying

--- P3G1DBP-SG1/DBUpdate.cs
+++ P3G1DBP-SG1/DBUpdate.cs
@@ -1,12 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace WindowsFormsApp1
+using System.Web;
+using System.IO;
+using System.Data.SqlClient;
+using MySql.Data.MySqlClient;
+namespace Project3Groep1
 {
-    class Class1
+    class DBUpdate
     {
+        public void update()
+        { 
+        string script = File.ReadAllText(@"P3G1.sql");
+        MySqlCommand cmd = new MySqlCommand(script);
+        cmd.ExecuteNonQuery;
+        }
     }
 }
+
