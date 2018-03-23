774929dca696370b26326d4a9e2007932e68d891
butatensei <31736483+butatensei@users.noreply.github.com>
2018-03-23 18:45:29 +0100

    Weer is nog beter

--- DATA/Fix this csv.py
+++ DATA/Fix this csv.py
@@ -1,8 +1,15 @@
-fileIn1 = 'KNMI_20180322_hourly.csv'
+fileIn1 = 'weerRo.csv'
 fileOut1 = 'FixedThatForYou.csv'
 column1 = 1
 column2 = 0
 KNMI = True
+dateFix = False
+dagJaar = 0
+temperatuurGem = 0
+hourCounter = 0
+monthCounter = 1
+fixTemp = 1
+rows = []
 
 import csv
 
@@ -26,16 +33,16 @@ with open(weatherStations) as csvweatherStations:
             reader = csv.reader(csvfileIn1)
 
             for row in reader:
-
-                if row[column1][2] == "/" and len(row) > 1:
-                    row[column1] = row[column1].replace("/", "")
-                    date = [row[column1][:2], row[column1][2:4], row[column1][4:]]
-                elif row[column1][1] == "/" and len(row) > 0:
-                    row[column1] = row[column1].replace("/", "")
-                    date = [row[column1][:1], row[column1][1:3], row[column1][3:]]
-                else:
-                    date = [row[column1][:4], row[column1][4:6], row[column1][6:]]
-                row = row[:column1] + date + row[(column1+1):]
+                if dateFix:
+                    if row[column1][2] == "/" and len(row) > 1:
+                        row[column1] = row[column1].replace("/", "")
+                        date = [row[column1][:2], row[column1][2:4], row[column1][4:]]
+                    elif row[column1][1] == "/" and len(row) > 0:
+                        row[column1] = row[column1].replace("/", "")
+                        date = [row[column1][:1], row[column1][1:3], row[column1][3:]]
+                    else:
+                        date = [row[column1][:4], row[column1][4:6], row[column1][6:]]
+                    row = row[:column1] + date + row[(column1+1):]
                 for i in range(len(row)):
                     row[i] = row[i].strip(" ")
                 for i in range(len(row)):
@@ -44,8 +51,27 @@ with open(weatherStations) as csvweatherStations:
                 if row[0] in stationDict:
                     row[0] = stationDict[row[0]]
 
-                if row[0] == "ROTTERDAM" or row[0] == "Station":
-                    print(row)
+                if row[0] == "Jaar":
+                    row.append("DagJaar")
+                    row.append("TemperatuurGem")
+                else:
+                    if hourCounter == 0:
+                        if not int(row[0]) == monthCounter:
+                            dagJaar += int(row[2])
+                            monthCounter = int(row[0])
+                        else:
+                            dagJaar += 1
+                    row.append(str(dagJaar))
+                    temperatuurGem += int(row[5])
+                    hourCounter += 1
+                    if hourCounter == 24:
+                        temperatuurGem = round(temperatuurGem/24)
+                        hourCounter = 0
+                        row.append(str(temperatuurGem))
+                        temperatuurGem = 0
+
+                if row[0] == "ROTTERDAM" or row[0] == "Station" or not dateFix:
+
 
                     with open(fileOut1, 'a',  newline='') as csvfileOut1:
                         writer = csv.writer(csvfileOut1)
